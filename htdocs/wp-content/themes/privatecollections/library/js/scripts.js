673290b78946518821203b968e617b0941686a84
Kyle <github@fishgate.co.za>
2014-06-23 14:47:53 +0200

    Did some shuffling around of page templates, getting ready to build the rest of the home page template.

--- htdocs/wp-content/themes/privatecollections/library/js/scripts.js
+++ htdocs/wp-content/themes/privatecollections/library/js/scripts.js
@@ -114,15 +114,17 @@ jQuery(document).ready(function($) {
   loadGravatars();
   
   // initiate supersized full screen background slideshow    
+  viewport = updateViewportDimensions();
+  
   var bg_slideshow_data = $.parseJSON(site_data.bg_slideshow);
   
-  $.supersized({
-      slide_interval : 1000,		// Length between transitions
-      transition : 1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-      transition_speed : 1000,		// Speed of transition
-      slides : bg_slideshow_data
-  });
-
-  
+  if (viewport.width >= 768) {
+    $.supersized({
+        slide_interval : 10000,		// Length between transitions
+        transition : 1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+        transition_speed : 1000,		// Speed of transition
+        slides : bg_slideshow_data
+    });
+  }
 
 }); /* end of as page load scripts */
